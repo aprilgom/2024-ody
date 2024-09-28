c81e8c716e7d06244e2d0ecb16bcb712d24c2262
aprilgom <aprilgom@gmail.com>
2024-09-28 17:51:46 +0900

    fd

--- android/app/src/androidTest/java/com/mulberry/ody/presentation/creation/MeetingCreationActivityTest.kt
+++ android/app/src/androidTest/java/com/mulberry/ody/presentation/creation/MeetingCreationActivityTest.kt
@@ -20,6 +20,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MeetingCreationActivityTest {
+    //aa
     @get:Rule
     val activityRule = ActivityScenarioRule(MeetingCreationActivity::class.java)
 
